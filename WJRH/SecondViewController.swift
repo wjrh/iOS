f48f40726f0507c163a0d9712adc4e02b681cf60
webere@lafayette.edu <admin@cognizance.us>
2016-06-23 11:42:17 -0700

    Initial Commit

---
+++ WJRH/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  WJRH
+//
+//  Created by Eric Weber on 6/23/16.
+//  Copyright © 2016 Eric Weber. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
